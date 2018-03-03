9076a6a1b306d431a44f41cc11de5a4a15836a5f
dylanbrandstetter <35245920+dylanbrandstetter@users.noreply.github.com>
2018-03-03 12:44:13 -0800

    Completed

---
+++ CST356 Week 7 Lab/CST356 Week 4 Lab/Controllers/BootstrapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CST356_Week_4_Lab.Controllers
+{
+    public class BootstrapController : Controller
+    {
+        // GET: Bootstrap
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
